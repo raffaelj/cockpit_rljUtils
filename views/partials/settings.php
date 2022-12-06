6a3ae4b8055557a81920af53fa335a7332c8b6d8
Raffael <github@raffaeljesche.de>
2022-12-06 12:26:34 +0100

    cleanup

--- views/partials/settings.php
+++ views/partials/settings.php
@@ -1,11 +1,11 @@
 
 <div>
-    <div class="uk-panel uk-panel-space uk-panel-box uk-panel-card">
-        <img src="@url('assets:app/media/icons/settings.svg')" width="50" height="50" alt="@lang('rljUtils')" />
+    <div class="uk-panel uk-panel-space uk-panel-box uk-panel-card uk-panel-card-hover">
+        <img src="@url('assets:app/media/icons/settings.svg')" width="50" height="50" alt="" />
 
         <div class="uk-text-truncate uk-margin">
-            @lang('rljUtils')
+            rljUtils
         </div>
-        <a class="uk-position-cover" aria-label="@lang('rljUtils')" href="@route('/rljutils')"></a>
+        <a class="uk-position-cover" aria-label="rljUtils" href="@route('/rljutils')"></a>
     </div>
 </div>
